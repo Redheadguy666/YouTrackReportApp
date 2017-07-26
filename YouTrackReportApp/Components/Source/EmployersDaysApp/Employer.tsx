9b89f2db341b3017b87aeb5a66104b48d77f8237
Slepov.V <Slepov.V@109PC0087>
2017-07-26 16:00:14 +0300

    .

---
+++ YouTrackReportApp/Components/Source/EmployersDaysApp/Employer.tsx
@@ -0,0 +1,13 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+export class Employer extends React.Component<{}, {}>
+{
+    render() {
+        return (
+            <div>
+                <h1>А вот это уже другая страница!</h1>
+            </div>
+        );
+    }
+}
---
+++ YouTrackReportApp/Components/Source/SummaryApp/SummaryInformationApp.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { ReportNavbar } from "../SummaryApp/ReportNavbar";
+import { ReportContent } from "../SummaryApp/ReportContent";
+
+export class SummaryInformationApp extends React.Component<{}, {}>
+{
+    render() {
+        return (
+            <div>
+                <ReportNavbar />
+                <ReportContent />
+            </div>
+        );
+    }
+}
